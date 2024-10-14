f5e327c283b58f9013ff4ed6ebe9702fb130ee48
saiteja-in <vurukondasaiteja13@gmail.com>
2024-10-14 22:07:56 +0530

    middleware for protecting routes and auth

---
+++ auth.config.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+import { LoginSchema } from "@/schemas";
+
+export default {
+  providers: [
+  ],
+} satisfies NextAuthConfig
