402b6fab866f1f599b11d0be8ac71d37f6fb1062
saiteja-in <vurukondasaiteja13@gmail.com>
2024-10-23 21:44:21 +0530

    custom spinner

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -3,13 +3,15 @@
 import { useCallback, useEffect, useState } from "react";
 import { BeatLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
-
+import { useTheme } from "next-themes";
 import { newVerification } from "@/actions/new-verification";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
+import { FaSpinner } from "react-icons/fa";
 
 export const NewVerificationForm = () => {
+  const theme=useTheme()
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
 
@@ -48,7 +50,7 @@ export const NewVerificationForm = () => {
     >
       <div className="flex items-center w-full justify-center">
         {!success && !error && (
-          <BeatLoader />
+          <FaSpinner className="animate-spin"size={"25"} />
         )}
         <FormSuccess message={success} />
         {!success && (
