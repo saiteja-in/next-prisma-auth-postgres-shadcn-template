9047cd55e0561b00286b60d4e3268fd9ef47aba8
saiteja-in <vurukondasaiteja13@gmail.com>
2024-10-26 02:08:08 +0530

    avatar component enabled

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,29 +1,39 @@
-import { auth, signOut } from '@/auth'
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-
-
-const page = async() => {
-    const session=await auth()
-    console.log(session)
+const page = async () => {
+  const session = await auth();
+  console.log(session);
   return (
-    <div className='items-center justify-center min-h-screen'>
-      <div className='flex flex-col gap-3'>
+    <div className="items-center justify-center min-h-screen">
+      <div className="flex flex-col gap-3">
         <p>Role: {session?.user?.role}</p>
         <p>Email: {session?.user?.email}</p>
         <p>Name: {session?.user?.name}</p>
         <p>OAuth: {session?.user?.isOAuth}</p>
-        <p>OAuth: {session?.user?.isTwoFactorEnabled}</p>
+        <p>Image: {session?.user?.image}</p>
+        <Avatar>
+          {session?.user?.image ? (
+            <AvatarImage src={session.user.image} />
+          ) : (
+            <AvatarFallback>
+              {session?.user?.name?.substring(0, 2).toUpperCase() ?? "AA"}
+            </AvatarFallback>
+          )}
+        </Avatar>
       </div>
-      <form action={async()=>{
-        "use server"
-        await signOut({ redirectTo: "/auth/login" });
-      }}>
-        <Button type='submit'>Sign out</Button>
+      <form
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: "/auth/login" });
+        }}
+      >
+        <Button type="submit">Sign out</Button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
