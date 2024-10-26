e0699e9e1ea897b9e1fa4cdf332410e30c02f860
saiteja-in <vurukondasaiteja13@gmail.com>
2024-10-27 01:06:01 +0530

    render user on client

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Github, Twitter, Linkedin } from "lucide-react";
 import Link from "next/link";
 
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,39 +1,58 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+// import { auth, signOut } from "@/auth";
+// import { Button } from "@/components/ui/button";
+// import React from "react";
+// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-const page = async () => {
-  const session = await auth();
-  console.log(session);
+// const page = async () => {
+//   const session = await auth();
+//   console.log(session);
+//   return (
+//     <div className="items-center justify-center min-h-screen">
+//       <div className="flex flex-col gap-3">
+//         <p>Role: {session?.user?.role}</p>
+//         <p>Email: {session?.user?.email}</p>
+//         <p>Name: {session?.user?.name}</p>
+//         <p>OAuth: {session?.user?.isOAuth}</p>
+//         <p>Image: {session?.user?.image}</p>
+//         <Avatar>
+//           {session?.user?.image ? (
+//             <AvatarImage src={session.user.image} />
+//           ) : (
+//             <AvatarFallback>
+//               {session?.user?.name?.substring(0, 2).toUpperCase() ?? "AA"}
+//             </AvatarFallback>
+//           )}
+//         </Avatar>
+//       </div>
+//       <form
+//         action={async () => {
+//           "use server";
+//           await signOut({ redirectTo: "/auth/login" });
+//         }}
+//       >
+//         <Button type="submit">Sign out</Button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default page;
+
+
+"use client"
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
+const SettingsClientPage=()=>{
+  const user=useCurrentUser()
+  console.log(user)
+  const onClick=()=>{
+    logout()
+  }
   return (
-    <div className="items-center justify-center min-h-screen">
-      <div className="flex flex-col gap-3">
-        <p>Role: {session?.user?.role}</p>
-        <p>Email: {session?.user?.email}</p>
-        <p>Name: {session?.user?.name}</p>
-        <p>OAuth: {session?.user?.isOAuth}</p>
-        <p>Image: {session?.user?.image}</p>
-        <Avatar>
-          {session?.user?.image ? (
-            <AvatarImage src={session.user.image} />
-          ) : (
-            <AvatarFallback>
-              {session?.user?.name?.substring(0, 2).toUpperCase() ?? "AA"}
-            </AvatarFallback>
-          )}
-        </Avatar>
-      </div>
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: "/auth/login" });
-        }}
-      >
-        <Button type="submit">Sign out</Button>
-      </form>
+    <div>
+      {JSON.stringify(user)}
+      <button onClick={onClick} type="submit">Sign Out</button>
     </div>
-  );
-};
-
-export default page;
+  )
+}
+export default SettingsClientPage
